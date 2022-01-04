0bf589f4e921281bcb2c9724b24bb3dc7bd23152
nopekun <51075690+toufiqnuur@users.noreply.github.com>
2022-01-04 22:07:51 +0700

    update container breakpoint

--- components/Container.js
+++ components/Container.js
@@ -1,3 +1,3 @@
 export default function Container({ className, children }) {
-  return <div className={`${className} container px-4 md:px-0 mx-auto`}>{children}</div>
+  return <div className={`${className} container lg:max-w-screen-lg px-4 md:px-0 mx-auto`}>{children}</div>
 }
